eed3e3b27d44cf43a11392cec79e5097c65973c4
Corn3 <robineliasson93@gmail.com>
2021-08-18 15:19:15 +0200

    Added translation page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import "./App.css";
+import {
+    BrowserRouter,
+    Switch,
+    Route,
+    NavLink
+} from "react-router-dom";
+import Startup from "./components/StartupWindow/Startup";
+import Translation from "./components/TranslationWindow/Translation";
+import NotFound from "./components/NotFound/NotFound";
+import AppContainer from "./hoc/AppContainer";
+
 const App = () => {
     return (
-        <div>
-            <h1>
-                TEST
-            </h1>
-        </div>
+        <BrowserRouter>
+            <AppContainer>
+                <div>
+                    <h1>
+                        TEST
+                    </h1>
+                </div>
+                <main>
+                    <Switch>
+                        <Route path="/" exact component={Startup} />
+                        <Route path="/translation" component={Translation} />
+                        <Route path="*" component={NotFound} />
+                    </Switch>
+                </main>
+            </AppContainer>
+        </BrowserRouter>
     );
 }
 
--- src/components/TranslationWindow/Translation.js
+++ src/components/TranslationWindow/Translation.js
@@ -1,9 +1,41 @@
+import { useEffect, useState } from "react";
+import { getTextData, postTextData } from "../../api/translation/translationAPI";
+import Sign from "./Sign";
+
 const Translation = () => {
+
+    const [words, setWords] = useState("");
+    const [symbols, setSymbols] = useState([]);
+    const DEFAULT_PATH = `${process.env.PUBLIC_URL}/assets/images/`
+
+    const translateButtonClick = () => {
+        setSymbols(words.split(""));
+    }
+
+    const onInputChange = event => {
+        setWords(event.target.value)
+    }
+
     return (
         <div className="Translation">
-
+            <label htmlFor="words">Text to be translated</label>
+            <div className="text-field-container">
+                <span className="input-symbol">⌨|</span>
+                <input id="words" className="text-size-m input-text-field" placeholder="Enter a text to be translated (Max 40 letters)" maxLength="40" onChange={onInputChange}></input>
+                <button id="text-button" className="input-button" onClick={translateButtonClick}>GO</button>
+            </div>
+            <div className="translation-field-container">
+                {
+                    symbols.map((s) => <Sign symbol={DEFAULT_PATH + s + ".png"} />)
+                }
+            </div>
         </div>
     );
 }
+
+/**
+ * setStorage("user", user);
+ * getStorage("user")
+ */
 
 export default Translation;
