1c14162fe0e1e2660a766a4fa83889f07b1f57fc
Corn3 <robineliasson93@gmail.com>
2021-08-19 14:56:29 +0200

    Added posts displaying on page

---
+++ src/components/ProfileWindow/Post.js
@@ -0,0 +1,11 @@
+const Post = props => {
+
+    return (
+        <li>
+            {props.post.text}
+        </li>
+    )
+
+}
+
+export default Post;
--- src/components/ProfileWindow/Profile.js
+++ src/components/ProfileWindow/Profile.js
@@ -1,12 +1,18 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import { useState } from 'react';
 import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { getTextDataWithId } from '../../api/translation/translationAPI';
+import { getUserData } from '../../api/user/userAPI';
 import { getStorage, removeItemStorage } from '../../storage';
+import Post from "./Post"
 
 const Profile = (props) => {
     const history = useHistory();
+
+    const [posts, setPosts] = useState([]);
 
     function handleLogout() {
         removeItemStorage("username");
@@ -16,14 +22,27 @@ const Profile = (props) => {
     useEffect(() => {
         if (!getStorage("username")) {
             history.push('/');
+        } else {
+            getUserPosts();
         }
     })
+
+    async function getUserPosts() {
+        const userId = (await getUserData(getStorage("username")))[0].id;
+        const p = (await getTextDataWithId(userId));
+        setPosts(p);
+    }
 
     return (
         <div className="Profile">
             <h1>Your Profile</h1>
-            <button id="clearTranslationsBtn"  className="btn btn-danger" >Clear history</button>
-            <button  onClick={handleLogout} className="btn btn-secondary">Sign out</button>
+            <button id="clearTranslationsBtn" className="btn btn-danger" >Clear history</button>
+            <button onClick={handleLogout} className="btn btn-secondary">Sign out</button>
+            <ol>
+                {
+                    posts.map((s, i) => <Post key={i} post={s} />)
+                }
+            </ol>
         </div>
     );
 }
